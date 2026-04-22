03d4f58a9ab0273ef7aac4d12d63c64f48aacb15
dshlychkou <4901549+dshlychkou@users.noreply.github.com>
2026-04-22 19:48:10 +0200

    feat: pgqs database migration

---
+++ pkg/pgqsdb/pgqsdb.go
@@ -0,0 +1,38 @@
+package pgqsdb
+
+import (
+	"context"
+	"embed"
+
+	"github.com/barnowlsnest/pgqs-harness/mgr"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+)
+
+//go:embed migrations/*.sql
+var embeddedMigrations embed.FS
+
+// RollOut applies pgqs database migrations.
+func RollOut(ctx context.Context, dbURL string) error {
+	driver, err := iofs.New(embeddedMigrations, "./migrations")
+	if err != nil {
+		return err
+	}
+
+	return mgr.Up(ctx, &mgr.Config{
+		DBURL:       dbURL,
+		EmbeddedSRC: driver,
+	})
+}
+
+// RollDown rolls down pgqs database migrations.
+func RollDown(ctx context.Context, dbURL string) error {
+	driver, err := iofs.New(embeddedMigrations, "./migrations")
+	if err != nil {
+		return err
+	}
+
+	return mgr.Down(ctx, &mgr.Config{
+		DBURL:       dbURL,
+		EmbeddedSRC: driver,
+	})
+}
